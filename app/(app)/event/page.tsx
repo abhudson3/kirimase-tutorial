8809d58749ccccb2c3dcb7a429a6d27a49397133
Chase Callahan <ctcallahan2@crimson.ua.edu>
2024-03-03 04:27:42 -0600

    rendering basic details for event

--- app/(app)/event/page.tsx
+++ app/(app)/event/page.tsx
@@ -6,6 +6,13 @@ export default async function EventPage() {
     const { session } = await getUserAuth()
 
     const events = await db?.event.findMany({
+        include: {
+            hrLead: {
+                include: {
+                    user: true
+                }
+            }
+        }
 
     })
 
@@ -13,7 +20,7 @@ export default async function EventPage() {
         <h1 className="text-2xl font-bold my-2">Profile</h1>
 
         {events?.map(({ date, description, eventId, hrLead, location, title }) => <div>
-            <p>{description} {date?.toDateString()} {eventId} {hrLead} {location} {title}</p>
+            <p>{description} {date?.toDateString()} {eventId} {hrLead?.user?.firstName} {location} {title}</p> <LinkButton label="Details" to={`/event/${eventId}`}/> 
         </div>)}
 
         <LinkButton to="/event/create" label="Create Event" />
